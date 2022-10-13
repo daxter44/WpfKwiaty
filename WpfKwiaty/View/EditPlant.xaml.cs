8a8be80f854d253fad04def4c1ac2f09760a4a32
Paulina Marcinkowska <paulina.marcinkovska@icloud.com>
2022-10-13 20:23:04 +0200

    repair of errors. Part2

---
+++ WpfKwiaty/View/EditPlant.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfKwiaty.Models;
+using WpfKwiaty.ViewModels;
+
+namespace WpfKwiaty
+{
+    /// <summary>
+    /// Logika interakcji dla klasy EditPlant.xaml
+    /// </summary>
+    public partial class EditPlant : Window
+    {    
+
+        public EditPlant(Plant plant)
+        {
+            InitializeComponent();
+            DataContext = new EditPlantViewModel(plant);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}

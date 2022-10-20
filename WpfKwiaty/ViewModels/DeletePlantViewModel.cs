74b875818bc53135f38ca4e10a1d2d1d29c80a2b
Paulina Marcinkowska <paulina.marcinkovska@icloud.com>
2022-10-20 18:58:04 +0200

    upload

---
+++ WpfKwiaty/ViewModels/DeletePlantViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.IO;
+using System.Windows;
+using WpfKwiaty.Models;
+using MvvmArchitecture;
+
+namespace WpfKwiaty.ViewModels
+{
+    class DeletePlantViewModel : ViewModelBase
+    {
+        private Plant _plantToDelete;
+        public Plant PlantToDelete
+        {
+            get
+            {
+                return _plantToDelete;
+            }
+            set
+            {
+                _plantToDelete = value;
+            }
+
+        }
+        public DeletePlantViewModel(Plant PlantToDelete)
+        {
+            this.PlantToDelete = PlantToDelete;
+        }
+        public RelayCommand DeletePlantCommand { get; private set; }
+
+
+    }
+
+}
